24006403a8696ebdc8bfe513c879d7b5de2fef86
Justin E. Samuels <js1599.msu@gmail.com>
2015-09-16 16:42:39 -0500

    Added delta variables for change of cursor, and cut sprite files into individual pieces

--- gulp/html/app.js
+++ gulp/html/app.js
@@ -1,12 +1,26 @@
 var Knob = React.createClass({
   getInitialState: function(){
-    return{position: 0, wheel: 0};
+    return{season: 'fall'};
   },
-
+  
+  click: function(data){
+     x = data.clientX;
+     y = data.clientY;
+     if ((x > 1025 && x > 1088) && (y > 497 && y < 560)){
+       console.log("fall");
+     }
+     else{
+     console.log("No");
+     }
+     this.setState({season: 'spring'});
+     React.findDOMNode(this.refs.season).src = "../style/img/Seasons/sprites_cut/" + this.state.season + ".png";
+  },
+  
   render: function(){
     return(
            <div>
              <img src = "../style/img/wood-bg.png"/>
+             <img src = "../style/img/Seasons/sprites_cut/fall.png" ref = "season" onClick = {this.click}/>
            </div>
     );//Ed
   } //end render function
