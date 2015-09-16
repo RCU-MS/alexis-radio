a7998c0f47b99a3886d45355f7ac941c776ecb14
Justin E. Samuels <js1599.msu@gmail.com>
2015-09-16 09:49:55 -0500

    Test branch for gulp.js

---
+++ app/app.js
@@ -0,0 +1,19 @@
+var Knob = React.createClass({
+  getInitialState: function(){
+    return{position: 0, wheel: 0};
+  },
+
+    knobTurn: function(){
+       this.
+         
+    },
+
+  render: function(){
+    return(
+           
+
+    );//Ed
+  } //end render function
+}); //end knob class
+
+React.render(<Knob />, mountNode);
